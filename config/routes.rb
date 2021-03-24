a98293e872f976384e1065f45c1c3c5a04a424fa
Wyattwicks <wyattwicks@gmail.com>
2021-03-24 15:03:27 -0600

    Add initial route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #main webpage route
+  get '/', to 'welcome#index'
+
+  #wyatt routes
+
+
+
+
+
+  #kevin routes
+
+
+
+  
 end
