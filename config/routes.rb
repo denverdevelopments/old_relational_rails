02c8573d040067ef4620afee918c1f626c7c7686
Wyattwicks <wyattwicks@gmail.com>
2021-03-24 15:23:00 -0600

    Completed migration of supplier table

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   #main webpage route
-  get '/', to 'welcome#index'
+  get '/', to: 'welcome#index'
 
   #wyatt routes
 
@@ -14,5 +14,5 @@ Rails.application.routes.draw do
 
 
 
-  
+
 end
