38236646f0ac19ec6a06ae5052399d4fe6295c00
Wyattwicks <wyattwicks@gmail.com>
2021-03-26 15:38:41 -0600

    Create supply side show

--- app/controllers/supply_trucks_controller.rb
+++ app/controllers/supply_trucks_controller.rb
@@ -2,4 +2,9 @@ class SupplyTrucksController < ApplicationController
   def index
     @supply_trucks = SupplyTruck.all
   end
+
+  def show
+    @supply_truck = SupplyTruck.find(params[:id])
+  end
+  
 end
