5e308819eb9c518d1d828e021ae258166bc0b5a1
Wyattwicks <wyattwicks@gmail.com>
2021-03-30 10:02:16 -0600

    Add routes and views for supply_trucks pages

--- app/controllers/supply_trucks_controller.rb
+++ app/controllers/supply_trucks_controller.rb
@@ -7,4 +7,41 @@ class SupplyTrucksController < ApplicationController
     @supply_truck = SupplyTruck.find(params[:id])
   end
 
+  def create
+    supply_truck = SupplyTruck.new({
+      driver_name: params[:driver_name],
+      number_of_daily_deliveries: params[:number_of_daily_deliveries],
+      refrigerated: params[:refrigerated]
+      })
+      supply_truck.save
+      redirect_to '/supply_trucks'
+  end
+
+  def update
+    supply_truck = SupplyTruck.find(params[:id])
+    supply_truck.update({
+      driver_name: params[:driver_name],
+      number_of_daily_deliveries: params[:number_of_daily_deliveries],
+      refrigerated: params[:refrigerated]
+      })
+      supply_truck.save
+      redirect_to '/supply_trucks'
+  end
+
+  def destroy
+    SupplyTruck.destroy(params[:id])
+    redirect_to 'supply_trucks'
+  end
+
+  def edit
+    @supply_truck = SupplyTruck.find(params[:id])
+  end
+
+  def new
+  end
+
+  def true
+    @supply_trucks = SupplyTruck.where(params[:refrigerated] == true)
+  end
+
 end
