9ad6bcfd9c17a4692fc89f96dfd435384f60ae94
kevn7n <kevn_nguyen@yahoo.com>
2021-03-31 21:34:52 -0600

    Add test for spec in restaurants

---
+++ spec/features/restaurants/edit_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe 'As a visitor' do
+  describe 'I visit a restaurant show page' do
+    it 'then I see that restaurant and attributes' do
+      tgik = Restaurant.create!(name: "TGI Kevin's", capacity: 220, has_bar: false)
+
+      visit "/restaurants/#{tgik.id}"
+      expect(page).to have_content(tgik.name)
+      expect(page).to have_content(tgik.id)
+      expect(page).to have_content(tgik.capacity)
+
+    end
+
+    it 'I see a link to update' do
+      tgik = Restaurant.create!(name: "TGI Kevin's", capacity: 220, has_bar: false)
+
+      visit "/restaurants/#{tgik.id}"
+      click_link 'Update Restaurant'
+      expect(current_path).to eq("/restaurants/#{tgik.id}/edit")
+    end
+
+      # expect(page).to have_link(". . see chefs", href: "/restaurants/#{tgik.id}/chefs")
+      # expect(page).to have_link("Update Restaurant", href: "/restaurants/#{tgik.id}/edit")
+      #
+      # visit '/'
+      # click_link 'Restaurants & Chefs'
+      # expect(current_path).to eq("/eats")
+  end
+end
---
+++ spec/features/restaurants/new_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe 'New Restaurant' do
+  describe 'As a visitor' do
+    describe 'When I visit the new restaurant form' do
+      it 'I can create a new restaurant' do
+        visit '/restaurants'
+        
+        click_link 'OPEN a new Restaurant'
+        expect(current_path).to eq('/restaurants/new')
+
+        fill_in 'Name', with: 'Fine Diner'
+        click_on 'Open Restaurant'
+
+        expect(current_path).to eq("/restaurants")
+        expect(page).to have_Content('Fine Diner')
+      end
+    end
+  end
+end
